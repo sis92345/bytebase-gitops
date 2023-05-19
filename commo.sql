08804cc988d4514656094c3e0b46f610361ceecf
Ted <anjfqhk1014@naver.com>
2023-05-19 17:36:49 +0900

    ㅁㄴㅇㅁㄴ

--- commo.sql
+++ commo.sql
@@ -1,3 +1,3 @@
 INSERT INTO TA_TEST ( id, course ) values ( 1, '국어' );
 INSERT INTO TA_TEST ( id, course ) values ( 2, '수학' );
-INSERT INTO TA_TEST ( id, course ) values ( 3, '존야' );
+INSERT INTO TA_TEST ( id, course ) values ( 3, '영어' );
