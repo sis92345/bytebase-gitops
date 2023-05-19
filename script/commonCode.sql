b1d631c280fcd7d6f73801c9a82f96510c68f2ef
Ted <anjfqhk1014@naver.com>
2023-05-19 17:42:21 +0900

    ㅁㄴㅇ

--- script/commonCode.sql
+++ script/commonCode.sql
@@ -1,3 +1,4 @@
 INSERT INTO TA_TEST ( id, course ) values ( 1, '국어' );
 INSERT INTO TA_TEST ( id, course ) values ( 2, '수학' );
 INSERT INTO TA_TEST ( id, course ) values ( 3, '존야' );
+INSERT INTO TA_TEST ( id, course ) values ( 4, '않이' );
